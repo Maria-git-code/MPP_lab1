b01bec4027cf56f75e308f5b62821f3621a414f3
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-10-31 11:31:59 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(10);
+
+            for (int i = 0; i < 20; i++)
+            {
+                taskQueue.EnqueueTask(Test);
+            }
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("TestTask in thread #" + Thread.CurrentThread.Name);
+            Thread.Sleep(6000);
+            
+        }
+    }
+}
---
+++ lab1/TaskQueue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace lab1
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+
+        private readonly Queue<TaskDelegate> _tasksQueue = new();
+
+        public TaskQueue(int threadsCount)
+        {
+            var threads = new Thread[threadsCount];
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                var thread = new Thread(Process) {Name = $"{i + 1}"};
+                thread.Start();
+                threads[i] = thread;
+            }
+        }
+
+        private void Process()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                TaskDelegate task = null;
+                lock (_tasksQueue)
+                {
+                    if (_tasksQueue.Count > 0)
+                    {
+                        task = _tasksQueue.Dequeue();
+                    }
+                }
+                task?.Invoke();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (_tasksQueue)
+            {
+                _tasksQueue.Enqueue(task);
+            }
+        }
+    }
+}
